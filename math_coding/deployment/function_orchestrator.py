87e9e14f1d927169a42ed26de267ecef1b0fdb64
ritesh.modi <rimod@microsoft.com>
2024-11-19 13:38:33 -0800

    linting

--- math_coding/deployment/function_orchestrator.py
+++ math_coding/deployment/function_orchestrator.py
@@ -3,10 +3,48 @@ import os
 import azure.functions as func
 import json
 from typing import Dict, Any
-
+from azure.core.settings import settings
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from azure.ai.inference.tracing import AIInferenceInstrumentor
+from azure.monitor.opentelemetry import configure_azure_monitor
 
 # Blueprint creation
 bp = func.Blueprint()
+
+
+# Enable instrumentation and logging of telemetry to the project
+def enable_telemetry(log_to_project: bool = False):
+    settings.tracing_implementation = "opentelemetry"
+    AIInferenceInstrumentor().instrument()
+
+    # enable logging message contents
+    os.environ["AZURE_TRACING_GEN_AI_CONTENT_RECORDING_ENABLED"] = log_to_project
+
+    if log_to_project:
+        from azure.monitor.opentelemetry import configure_azure_monitor
+
+        project = AIProjectClient.from_connection_string(
+            conn_str=os.environ["CONNECTION_STRING"], credential=DefaultAzureCredential()
+        )
+        #project.telemetry.enable(destination=None)
+        #tracing_link = f"https://ai.azure.com/tracing?wsid=/subscriptions/{project.scope[os.environ['SUBSCRIPTION_ID']]}/resourceGroups/{project.scope[os.environ['RESOURCE_GROUP_NAME']]}/providers/Microsoft.MachineLearningServices/workspaces/{project.scope[os.environ['PROJECT_NAME']]}"
+        application_insights_connection_string = project.telemetry.get_connection_string()
+        if not application_insights_connection_string:
+            logging.warning(
+                "No application insights configured, telemetry will not be logged to project. Add application insights at:"
+            )
+            #logging.warning(tracing_link)
+
+            return
+
+        configure_azure_monitor(connection_string=application_insights_connection_string)
+        logging.info("Enabled telemetry logging to project, view traces at:")
+        #logging.info(tracing_link)    
+
+
+
+
 
 
 @bp.route(route="process-math")
