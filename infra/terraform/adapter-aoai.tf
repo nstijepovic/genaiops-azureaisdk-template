9dd4c7e6a9721a7d33a9f54d847d723dc53415a2
nstijepovic <nstijepovic@microsoft.com>
2025-04-09 11:01:59 +0400

    update name

--- infra/terraform/adapter-aoai.tf
+++ infra/terraform/adapter-aoai.tf
@@ -13,7 +13,7 @@ module "aoai" {
   source              = "./AOAI"
   name                = "aoai-${each.key}-${random_string.aoai_suffix[each.key].result}"
   location            = var.resource_group_location
-  resource_group_name = azurerm_resource_group.main.name
+  resource_group_name = azurerm_resource_group.rg.name
   sku_name            = each.value.sku_name
 
   deployments         = lookup(var.azure_openai_deployments, each.key, {})
